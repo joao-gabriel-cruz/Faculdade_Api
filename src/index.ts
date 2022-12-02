371ae2bae8576968a580face25da7bc724681340
joao-gabriel-cruz <joaogabrielpinhodacruz@gmail.com>
2022-12-01 22:23:13 -0300

    :bug: ajuste de porta

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import { userRouter } from './routers/users/';
-
+const port = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 app.use(express.json());
 
-app.get('/', (request, response) => {
+app.get('/', (_, response) => {
   response.send('Hello World');
 });
 
 app.use('/users', userRouter);
 
-app.listen(5000, () => console.log('Server is running 🚀'));
+app.listen(port, () => console.log('Server is running 🚀'));
