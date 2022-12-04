757903e76c95ad27bc6d9dc56e01a8ceb99afa45
joao-gabriel-cruz <joaogabrielpinhodacruz@gmail.com>
2022-12-04 12:29:11 -0300

    :zap: arquivo de tipagem de Students

---
+++ src/@types/students.d.ts
@@ -0,0 +1,11 @@
+export type StudentsType = {
+  registration: string;
+  name: string;
+  course: string;
+  cpf: string;
+  email: string;
+  street: string;
+  street_number: number;
+  district: string;
+  cep: string;
+};
