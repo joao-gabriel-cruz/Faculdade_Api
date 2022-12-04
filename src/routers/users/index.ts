29da3b7c66f593f4f8aad827b4194d8391a10c94
joao-gabriel-cruz <joaogabrielpinhodacruz@gmail.com>
2022-12-04 12:52:07 -0300

    :sparkles: nova rota de login

--- src/routers/users/index.ts
+++ src/routers/users/index.ts
@@ -1,8 +1,31 @@
-import { studentRouter } from "./StudentRouter";
-import { Router } from "express";
+import { studentRouter } from './StudentRouter';
+import { Router } from 'express';
+import { Connection } from '../../database/connection';
 
 const userRouter = Router();
 
-userRouter.use("/students", studentRouter);
+userRouter.use('/students', studentRouter);
+
+userRouter.post('/login', async (request, response) => {
+  const role = request.body.role;
+
+  if (role === 'student') {
+    const { registration, password } = request.body;
+    const connection = new Connection();
+    const result = await connection.select('students', [
+      'registration',
+      'password',
+    ]);
+    if (
+      result[0].registration === registration &&
+      result[0].password === password
+    ) {
+      response.json(result);
+      console.log(result);
+    } else {
+      response.status(500).send('Internal server error');
+    }
+  }
+});
 
 export { userRouter };
